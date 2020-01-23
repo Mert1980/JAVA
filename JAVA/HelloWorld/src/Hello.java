df5eab552eaa8a5810c7d3fb64f7b318230b5a23
Mert Demirok <mertdemirok80@gmail.com>
2020-01-23 11:57:49 +0100

    basic math operators

--- JAVA/HelloWorld/src/Hello.java
+++ JAVA/HelloWorld/src/Hello.java
@@ -2,7 +2,7 @@ public class Hello {
     public static void main(String[] args){
         System.out.println("Hello World!");
 
-        int myFirstNumber = 5; // declaration statement
+        int myFirstNumber = (5+10) + (2*10); // declaration statement
         System.out.println(myFirstNumber);
     }
 }
