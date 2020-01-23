c26b3f419562474820692ea42c236aea96991cc4
Mert Demirok <mertdemirok80@gmail.com>
2020-01-23 11:54:44 +0100

    first declaration statement

--- JAVA/HelloWorld/src/Hello.java
+++ JAVA/HelloWorld/src/Hello.java
@@ -1,5 +1,8 @@
 public class Hello {
     public static void main(String[] args){
         System.out.println("Hello World!");
+
+        int myFirstNumber = 5; // declaration statement
+        System.out.println(myFirstNumber);
     }
 }
