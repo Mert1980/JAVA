d76d81f5802b8e05350b1e44b8a13d287dc4d02f
Mert Demirok <mertdemirok80@gmail.com>
2020-01-23 12:13:24 +0100

    combining multiple expressions

--- JAVA/HelloWorld/src/Hello.java
+++ JAVA/HelloWorld/src/Hello.java
@@ -3,6 +3,11 @@ public class Hello {
         System.out.println("Hello World!");
 
         int myFirstNumber = (5+10) + (2*10); // declaration statement
-        System.out.println(myFirstNumber);
+        int mySecondNumber = 12;
+        int myThirdNumber = myFirstNumber * 2;
+        int myTotal = myFirstNumber+mySecondNumber+myThirdNumber;
+        int myLastOne = 1000 - myTotal;
+
+        System.out.println(myLastOne);
     }
 }
