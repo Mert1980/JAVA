1bb28e7ff8ec779dd1c50441ab6ba9fe731997fd
Mert Demirok <mertdemirok80@gmail.com>
2020-01-25 13:41:41 +0100

    the difference between i++ and ++i

--- JAVA/HelloWorld/src/Hello.java
+++ JAVA/HelloWorld/src/Hello.java
@@ -2,12 +2,14 @@ public class Hello {
     public static void main(String[] args){
         System.out.println("Hello World!");
 
-        int myFirstNumber = (5+10) + (2*10); // declaration statement
-        int mySecondNumber = 12;
-        int myThirdNumber = myFirstNumber * 2;
-        int myTotal = myFirstNumber+mySecondNumber+myThirdNumber;
-        int myLastOne = 1000 - myTotal;
-
-        System.out.println(myLastOne);
+//        int myFirstNumber = (5+10) + (2*10); // declaration statement
+//        int mySecondNumber = 12;
+//        int myThirdNumber = myFirstNumber * 2;
+//        int myTotal = myFirstNumber+mySecondNumber+myThirdNumber;
+//        int myLastOne = 1000 - myTotal;
+//        System.out.println(myLastOne);
+        int number = 2;
+        System.out.println((number++ * 2));
+        System.out.println(++number);
     }
 }
