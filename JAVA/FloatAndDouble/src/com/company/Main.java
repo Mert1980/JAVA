72069ddab60fff08698f2124455d556b816e9875
Mert Demirok <mertdemirok80@gmail.com>
2020-02-02 09:10:44 +0100

    float and double types

---
+++ JAVA/FloatAndDouble/src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	float myMinFloatValue = Float.MIN_VALUE;
+	float myMaxFloatValue = Float.MAX_VALUE;
+        System.out.println("My minumum float value: "+ myMinFloatValue);
+        System.out.println("My maximum float value: "+ myMinFloatValue);
+
+    double myMinDoubleValue = Double.MIN_VALUE;
+    double myMaxDoubleValue = Double.MAX_VALUE;
+        System.out.println("My minumum double value: "+ myMinDoubleValue);
+        System.out.println("My maximum double value: "+ myMaxDoubleValue);
+
+    int myIntValue = 5;
+    float myFloatValue = 5.25f;
+    double myDoubleValue = 5.25d; // double is the default type
+
+
+    }
+}
