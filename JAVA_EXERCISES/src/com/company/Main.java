3dc72482d9802e024fc2941cc30d7cf334d2ec81
Mert Demirok <mertdemirok80@gmail.com>
2020-02-15 07:45:43 +0100

    Creates random values in different types

---
+++ JAVA_EXERCISES/src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Random generator = new Random(); // a random number generator
+        int x = generator.nextInt(); // a random number
+        System.out.println(x);
+        int y = generator.nextInt(100); // between 0 and 99 included
+        System.out.println(y);
+        boolean b = generator.nextBoolean();
+        System.out.println(b);
+        double z = generator.nextDouble();
+        System.out.println(z);
+
+    }
+}
