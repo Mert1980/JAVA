5257afbaff7d9a74e4464c2d81391174b6f0cd8b
Mert Demirok <mertdemirok80@gmail.com>
2020-02-15 07:57:17 +0100

    the program exits when the counter reaches a predefined number

--- SystemExitTest/src/com/company/Main.java
+++ SystemExitTest/src/com/company/Main.java
@@ -3,6 +3,15 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int i = 1;
+        while (i < 10) {
+            if (i == 5) {
+                System.out.println("Break");
+                System.exit(1);
+            }
+            System.out.println(i);
+            i++;
+        }
+        System.out.println("End of main");
     }
 }
